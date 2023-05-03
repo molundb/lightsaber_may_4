e1dd6599046f24126ba0867d09dcc8a27e07cd56
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-05-03 15:24:20 +0200

    feat: add background image and turned off lightsaber

---
+++ lib/repeated_image.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+import 'package:spritewidget/spritewidget.dart';
+
+class RepeatedImage extends Node {
+  late Sprite _sprite0;
+  late Sprite _sprite1;
+
+  RepeatedImage(Image image, [BlendMode? mode]) {
+    _sprite0 = Sprite.fromImage(image);
+    _sprite0.size = const Size(1024.0, 1024.0);
+    _sprite0.pivot = Offset.zero;
+    _sprite1 = Sprite.fromImage(image);
+    _sprite1.size = const Size(1024.0, 1024.0);
+    _sprite1.pivot = Offset.zero;
+    _sprite1.position = const Offset(0.0, -1024.0);
+
+    if (mode != null) {
+      _sprite0.blendMode = mode;
+      _sprite1.blendMode = mode;
+    }
+
+    addChild(_sprite0);
+    addChild(_sprite1);
+  }
+
+  void move(double dy) {
+    double yPos = (position.dy + dy) % 1024.0;
+    position = Offset(0.0, yPos);
+  }
+}
